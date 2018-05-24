1348f37d671d30231dd1bf3d91dea2c157179cd7
unknown <maxhuddleston@gmail.com>
2018-05-24 14:03:34 -0400

    Initial card layout

---
+++ app/src/main/java/com/credit_cure/creditcuremobile/CCAdapter.java
@@ -0,0 +1,12 @@
+package com.credit_cure.creditcuremobile;
+
+import android.support.v7.widget.RecyclerView;
+
+public class CCAdapter extends RecyclerView.Adapter<CCAdapter.ViewHolder> {
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
--- app/src/main/java/com/credit_cure/creditcuremobile/MainActivity.java
+++ app/src/main/java/com/credit_cure/creditcuremobile/MainActivity.java
@@ -2,12 +2,22 @@ package com.credit_cure.creditcuremobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView ccRecycclerView;
+    private RecyclerView.Adapter ccRecyclerAdapter;
+    private RecyclerView.LayoutManager ccLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ccRecycclerView = (RecyclerView) findViewById(R.id.cc_recycler_view);
+        ccLayoutManager = new LinearLayoutManager(this);
+        ccRecycclerView.setLayoutManager(ccLayoutManager);
     }
 }
