4055fcf7a9590591bfeae28138b14febce25fb82
unknown <maxhuddleston@gmail.com>
2018-05-24 15:28:41 -0400

    Recycler view in a stable, complete position.

--- app/src/main/java/com/credit_cure/creditcuremobile/CCAdapter.java
+++ app/src/main/java/com/credit_cure/creditcuremobile/CCAdapter.java
@@ -1,20 +1,62 @@
 package com.credit_cure.creditcuremobile;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class CCAdapter extends RecyclerView.Adapter<CCAdapter.ViewHolder> {
 
+    private ArrayList<VirtualCard> vcArray;
 
+    public CCAdapter(ArrayList<VirtualCard> vcArray) {
+        this.vcArray = vcArray;
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public CardView ccView;
+        private CardView ccView;
+        public TextView ccNumberView;
+        public TextView cvvDateView;
+        public TextView chargeAmountView;
 
-        public ViewHolder(CardView ccView) {
-            super(ccView);
-            this.ccView = ccView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ccView = (CardView) itemView.findViewById(R.id.cc_card_view);
+            ccNumberView = (TextView) itemView.findViewById(R.id.cardNumber);
+            cvvDateView = (TextView) itemView.findViewById(R.id.cvv_date_textview);
+            chargeAmountView = (TextView) itemView.findViewById(R.id.cc_charge_tb);
         }
+    }
+
+    @Override
+    public CCAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cc_card_layout,
+                parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder vh, int i) {
+        vh.ccNumberView.setText(vcArray.get(i).getCardNumber());
+        vh.cvvDateView.setText(vcArray.get(i).getCvv() + "  " + vcArray.get(i).getDate());
+        vh.chargeAmountView.setText(vcArray.get(i).getAmount());
+    }
+
+    @Override
+    public int getItemCount() {
+        return vcArray.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 }
--- app/src/main/java/com/credit_cure/creditcuremobile/MainActivity.java
+++ app/src/main/java/com/credit_cure/creditcuremobile/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView ccRecycclerView;
@@ -16,8 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayList<VirtualCard> vc = new ArrayList<VirtualCard>();
+        vc.add(new VirtualCard("1234 1234 1234 1234", "$78.12", "12/21", "512"));
+        vc.add(new VirtualCard("1234 1234 1234 1234", "$78.12", "12/21", "512"));
+        vc.add(new VirtualCard("1234 1234 1234 1234", "$78.12", "12/21", "512"));
+
         ccRecycclerView = (RecyclerView) findViewById(R.id.cc_recycler_view);
         ccLayoutManager = new LinearLayoutManager(this);
         ccRecycclerView.setLayoutManager(ccLayoutManager);
+
+        CCAdapter ccAdapter = new CCAdapter(vc);
+        ccRecycclerView.setAdapter(ccAdapter);
     }
 }
