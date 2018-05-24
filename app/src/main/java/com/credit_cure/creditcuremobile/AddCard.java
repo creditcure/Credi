7d75174a4b39f065148f8e7761147139dae41ae5
unknown <maxhuddleston@gmail.com>
2018-05-24 18:48:58 -0400

    Finished with office

---
+++ app/src/main/java/com/credit_cure/creditcuremobile/AddCard.java
@@ -0,0 +1,13 @@
+package com.credit_cure.creditcuremobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+    }
+}
